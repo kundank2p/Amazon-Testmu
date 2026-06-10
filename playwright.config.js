2333bd9ff8b4fd2cb02b8ccee396b9e5898d2c15
kundank2p <kundank2p@gmail.com>
2026-06-10 20:29:52 +0530

    Changed some configs

--- playwright.config.js
+++ playwright.config.js
@@ -3,24 +3,21 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 
-const __filename =fileURLToPath(import.meta.url);
-const __dirname =path.dirname(__filename);
-dotenv.config({path: path.resolve(__dirname,'.env')});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+dotenv.config({ path: path.resolve(__dirname, '.env') });
 
 export default defineConfig({
     testDir: './tests',
-    fullyParallel: true,
-    workers: 2,
+    fullyParallel: process.env.CI ? false : true,
+    workers: process.env.CI ? 1 : 2,
     timeout: 60000,
     use: {
-        baseURL:process.env.BASE_URL,
-        headless:process.env.HEADLESS === 'true',
-        screenshot:'only-on-failure',
-        video:'retain-on-failure',
-        trace:'retain-on-failure'
+        baseURL: process.env.BASE_URL,
+        headless: process.env.HEADLESS === 'true',
+        screenshot: 'only-on-failure',
+        video: 'retain-on-failure',
+        trace: 'retain-on-failure',
     },
-    reporter: [
-        ['html'],
-        ['list']
-    ]
+    reporter: [['html'], ['list']],
 });
